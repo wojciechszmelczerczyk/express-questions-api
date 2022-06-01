66c6c49c3b499ac7d0528f2250c293de7a11d3f6
Wojciech <szmelczerczykwojciech@gmail.com>
2022-06-01 13:49:34 +0200

    get question by id and create question unit tests implemented

--- repositories/question.test.js
+++ repositories/question.test.js
@@ -1,4 +1,4 @@
-const { writeFile, rm } = require('fs/promises')
+const { writeFile, readFile, rm } = require('fs/promises')
 const { faker } = require('@faker-js/faker')
 const { makeQuestionRepository } = require('./question')
 
@@ -39,5 +39,73 @@ describe('question repository', () => {
     await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
 
     expect(await questionRepo.getQuestions()).toHaveLength(2)
+  })
+
+  test('should return question by specific id', async () => {
+    // user id
+    let id = faker.datatype.uuid()
+
+    const testQuestions = [
+      {
+        id,
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+    expect(await questionRepo.getQuestionById(id)).toHaveLength(1)
+  })
+
+  test('should add new question', async () => {
+    // questions list
+    const testQuestions = [
+      {
+        id: faker.datatype.uuid(),
+        summary: 'What is my name?',
+        author: 'Jack London',
+        answers: []
+      },
+      {
+        id: faker.datatype.uuid(),
+        summary: 'Who are you?',
+        author: 'Tim Doods',
+        answers: []
+      }
+    ]
+
+    // questions list length
+    let questionsListLength = testQuestions.length
+
+    // new question
+    const newQuestion = {
+      id: faker.datatype.uuid(),
+      summary: 'How fast sound travel?',
+      author: 'Norman Kowalsky',
+      answers: []
+    }
+
+    // write base 2 questions to file
+    await writeFile(TEST_QUESTIONS_FILE_PATH, JSON.stringify(testQuestions))
+
+    // add new question
+    await questionRepo.addQuestion(newQuestion)
+
+    // read question list from file with new question added and parse to object
+    let listWithNewQuestion = JSON.parse(
+      await readFile(TEST_QUESTIONS_FILE_PATH, {
+        encoding: 'utf-8'
+      })
+    )
+
+    // expect new list length to be greater by 1 (new question added)
+    expect(listWithNewQuestion.length).toBe(questionsListLength + 1)
   })
 })
