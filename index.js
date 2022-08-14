c20b55f94fd59559417a20ff6e195daf6d365164
Wojciech <szmelczerczykwojciech@gmail.com>
2022-08-14 18:23:36 +0200

    use of env var port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const makeRepositories = require('./middleware/repositories')
 const { v4: uuidv4 } = require('uuid')
 
 const STORAGE_FILE_PATH = 'questions.json'
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 const app = express()
 
